a71ecab184b933592e5071e9c14579388745fcbb
GeKaixing <2890901420@qq.com>
2024-01-05 14:00:53 +0800

    添加鼠标选中文本会红

--- src/components/Article/ArticleContext.vue
+++ src/components/Article/ArticleContext.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { marked } from "marked";
-import dffffff from '../../../public/markdown/vue3文档速读.md?raw'
+import markdown from '../../../public/markdown/vue3文档速读.md?raw'
+import { Log } from '@icon-park/vue-next';
+const articleContext_box = ref(null)
 marked.setOptions({
     pedantic: false,
     gfm: true,
@@ -12,24 +14,45 @@ marked.setOptions({
     xhtml: false,
 })
 onMounted(() => {
-    const html = marked(dffffff)
-    document.querySelector('.articleContext_box').innerHTML=html
+    const html = marked(markdown)
+    articleContext_box.value.innerHTML = html
+    articleContext_box.value.onmouseup = (e) => {
+        const selection = window.getSelection()
+        const selectiontext = selection.toString()
+        const ranges = selection.getRangeAt(0);
+        if (selectiontext) {
+            const docObj = ranges.extractContents(); //移动了Range 中的内容从文档树到DocumentFragment（文档片段对象)。
+            console.log(docObj);
+            let dom = document.createElement('div');
+            dom.style.display='inline';
+            dom.style.color = 'red';
+            dom.style.fontSize = '16px';
+            dom.appendChild(docObj);
+            ranges.insertNode(dom);
+        }
+    }
+
 })
 </script>
 <template>
     <div class="articleContext">
-        <div class="articleContext_box">
+        <div class="articleContext_box" ref="articleContext_box">
         </div>
     </div>
 </template>
 <style>
+span::before {
+     content: ''; 
+     font-size: 0; 
+}
+
 .articleContext {
-    
+
     width: 813px;
     margin: 0 auto;
     margin-top: 16px;
     background: #D9D9D9;
-    
+
 }
 
 .articleContext_box {
